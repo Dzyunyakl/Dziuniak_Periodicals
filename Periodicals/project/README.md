cc5ba3feba6f7d9f4245eecf4612d4adf1f9d110
Lev <lev.dziuniak@sigma.software>
2020-05-15 16:03:17 +0300

    all files upload

---
+++ Periodicals/project/README.md
@@ -0,0 +1,1 @@
+# My java periodicals project.
---
+++ Periodicals/project/src/main/java/com/example/project/repositories/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.example.project.repositories;
+import java.util.List;
+import java.util.Optional;
+import com.example.project.entity.Users;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<Users, Integer> {
+    List<Users> findByLogin(String login);
+}
